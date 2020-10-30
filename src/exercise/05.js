79dcedcd59687b220751f5309b573e6042a51ce5
Adam Whitehurst <Adam.Whitehurst@live.com>
2020-10-30 13:18:21 -0700

    Init commit (already finished)

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,8 +1,8 @@
 // Styling
 // http://localhost:3000/isolated/exercise/05.js
 
-import React from 'react'
-import '../box-styles.css'
+import React from 'react';
+import '../box-styles.css';
 
 // 💰 Use the className for the size and style (backgroundColor) for the color
 // 💰 each of the elements should also have the "box" className applied
@@ -12,9 +12,11 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const baseStyle ={ fontStyle: 'italic'};
+const smallBox = <div className="box box--small" style={{...baseStyle, backgroundColor: 'lightblue'}}>small lightblue box</div>
+const smallBox2 = <div className="box box--small" style={{...baseStyle, backgroundColor: 'papayawhip', position: "relative", bottom: 180}}>small papayawhip box</div>
+const mediumBox = <div className="box box--medium" style={{...baseStyle, backgroundColor: 'pink'}}>medium pink box</div>
+const largeBox = <div className="box box--large" style={{...baseStyle, backgroundColor: 'orange'}}>large orange box</div>
 
 function App() {
   return (
@@ -22,6 +24,7 @@ function App() {
       {smallBox}
       {mediumBox}
       {largeBox}
+      {smallBox2}
     </div>
   )
 }
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,30 +1,27 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import React from 'react'
+import React from 'react';
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  // const ref = React.useRef();
+  const [username, setUsername] = React.useState('');
 
-  // 🐨 add the onSubmit handler to the <form> below
-
-  // 🐨 make sure to associate the label to the input. 
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  const onInputChange = (e) => setUsername(e.target.value)
+  // const isValid = username && username === username.toLowerCase();
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    alert(`You entered: ${username}`)
+  }
   return (
-    <form>
+    <form onSubmit={onFormSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username">Username:</label>
+        {/* <input ref={ref} id="username" type="text" /> */}
+        <input id="username" type="text" onChange={onInputChange} value={username} />
       </div>
-      <button type="submit">Submit</button>
+      <button /* disabled={!isValid} */ type="submit">Submit</button>
+        {/* {!isValid && <label role="alert">Username must be lowercase</label>} */}
     </form>
   )
 }
